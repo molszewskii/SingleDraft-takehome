49c3e8af26bb40074512b60c4154c8d095a30f4d
Michał <80591@student.pb.edu.pl>
2024-11-19 18:32:06 +0100

    Modify: Added CardState for managing expanded and deleted cards

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,11 @@
 import { create } from "zustand";
 
-type State = {};
-
-type Actions = {};
+type CardState ={
+    expandedCards: Set<number>;
+    deletedCards: Set<number>;
+    addExpandedCard: (id: number) => void;
+    removeExpandedCard: (id: number) => void;
+    addDeletedCard: (id: number) => void;
+}
 
 export const useStore = create<State & Actions>((set) => ({}));
