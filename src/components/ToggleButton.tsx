038a5f204cf84506018bf1d2154795497bc71129
Michał <80591@student.pb.edu.pl>
2024-11-19 19:24:26 +0100

    Implement ToggleButton component with dynamic styles based on active state

--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { FC } from "react";
 
 type ToggleButtonProps={
     onToggle: () => void;
     isActive: boolean;
     children: React.ReactNode;
 };
+
+export const ToggleButton: FC<ToggleButtonProps> = ({onToggle, isActive, children}) =>{
+    return(
+        <button
+            onClick={onToggle}
+            className={`transition-colors ${
+                isActive ? "bg-gray-800" : "bg-black/75"
+              } text-white text-sm rounded px-3 py-1`}   
+        >
+            {children}
+        </button>
+    )
+}
