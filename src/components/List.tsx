dc77afd3656dd06284b2e177742408ccb847b324
Michał <80591@student.pb.edu.pl>
2024-11-19 18:43:16 +0100

    Add expand and collapse functionality

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,26 +1,41 @@
 import { FC } from "react";
 import { ListItem } from "../api/getListData";
 import { DeleteButton, ExpandButton } from "./Buttons";
-import { ChevronUpIcon } from "./icons";
+import { ChevronDownIcon, ChevronUpIcon } from "./icons";
+import { useStore } from "../store";
 
 type CardProps = {
+  id: ListItem["id"];
   title: ListItem["title"];
   description: ListItem["description"];
 };
 
-export const Card: FC<CardProps> = ({ title, description }) => {
+export const Card: FC<CardProps> = ({id, title, description }) => {
+  const {expandedCards, addExpandedCard, removeExpandedCard, addDeletedCard} = useStore();
+
+  const isExpanded = expandedCards.has(id);
+
+  const toggleExpand = () =>{
+    if(isExpanded){
+      removeExpandedCard(id);
+    }else{
+      addExpandedCard(id);
+    }
+  }
+
   return (
     <div className="border border-black px-2 py-1.5">
       <div className="flex justify-between mb-0.5">
         <h1 className="font-medium">{title}</h1>
         <div className="flex">
-          <ExpandButton>
-            <ChevronUpIcon />
+          <ExpandButton onClick={toggleExpand}>
+            {isExpanded ? <ChevronDownIcon/> : <ChevronUpIcon />}
           </ExpandButton>
           <DeleteButton />
         </div>
       </div>
-      <p className="text-sm">{description}</p>
+      {isExpanded && <p className="text-sm">{description}</p>}
+      
     </div>
   );
 };
